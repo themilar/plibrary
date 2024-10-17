679612342b477b841456ad98522e210be162bdd2
Oluwadamilare <joeydre69@gmail.com>
2024-10-17 03:36:57 +0100

    json responses

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -12,7 +12,10 @@ func (app *application) bookCreate(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new book")
 }
 func (app *application) bookDetail(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
-
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 	fmt.Fprintf(w, "details of the book %d\n", id)
 }
--- cmd/api/healhcheck.go
+++ cmd/api/healhcheck.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"version":     version,
+		"environment": app.config.env,
+	}
+	resp, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	resp = append(resp, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+
 }
