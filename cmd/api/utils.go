432ddbdc53f5d66e06f6029c457e3f2f8d501088
Oluwadamilare <joeydre69@gmail.com>
2025-03-25 18:03:16 +0100

    restricting json input

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 // func (app *application) readIDParam(r *http.Request) (int64, error) {
@@ -36,8 +37,11 @@ func (app *application) writeJson(w http.ResponseWriter, status int, data envelo
 type envelope map[string]interface{}
 
 func (app *application) readJson(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-
-	err := json.NewDecoder(r.Body).Decode(dst)
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -49,6 +53,11 @@ func (app *application) readJson(w http.ResponseWriter, r *http.Request, dst int
 			return errors.New("body contains badly-formed JSON")
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http requet body too large":
+			return fmt.Errorf("body must be larger than %d bytes ", maxBytes)
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
 				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
@@ -61,6 +70,10 @@ func (app *application) readJson(w http.ResponseWriter, r *http.Request, dst int
 			return err
 		}
 
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }
