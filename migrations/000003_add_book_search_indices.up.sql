b5404f5ca06811ada1f7e89be88903ed9f7e61ca
Oluwadamilare <joeydre69@gmail.com>
2025-05-05 08:06:01 +0100

    remove unused migrate rule, correct the migration file

--- migrations/000003_add_book_search_indices.up.sql
+++ migrations/000003_add_book_search_indices.up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS books_title_idx ON movies USING GIN (to_tsvector('simple', title));
+CREATE INDEX IF NOT EXISTS books_title_idx ON books USING GIN (to_tsvector('simple', title));
